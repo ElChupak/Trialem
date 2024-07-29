e6e629e4c66ac004f06228acee8f29a0b4ae99a5
Dmytro <dmitrij.sergiencko15@gmail.com>
2024-07-30 00:02:45 +0200

    preadaptation save

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,18 +13,62 @@ let scroll = window.scrollY;
 });
 
 
+//Loader 
+
+const loader = document.querySelector('.loader')
+
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    loader.style.display = 'none';
+  }, 600)
+
+  // setTimeout(() => {
+  //   container.classList.add('show')
+  //   body.style.overflow = 'auto'
+  // }, 5500)
+})
+
+
 // Swiper Slider
 
 const swiper = new Swiper('.feedback .swiper', {
     slidesPerView: 2,
-    // loop: true,
+    // spaceBetween: 50,
+    // centeredSlides: true,
   
     navigation: {
       nextEl: '.slider-btn--next',
       prevEl: '.slider-btn--prev',
     },
-  
+    breakpoints: {
+      // when window width is >= 320px
+      350: {
+        slidesPerView: 1,
+        centeredSlides: true,
+      },
+      600: {
+        slidesPerView: 1,
+        centeredSlides: true,
+      },
+      992: {
+        slidesPerView: 2,
+      },
+    }
+
   });
+
+
+// Swiper Btn disabled
+// const swiperWrapper = document.querySelector('.swiper-wrapper');
+// const btnNext = document.querySelector('.slider-btn--next');
+
+// document.addEventListener('click', ()=>{
+//   if(swiperWrapper.style.transform == 'translate3d(-1800px, 0px, 0px)') {
+//     btnNext.setAttribute('disabled', true)
+
+//   }
+// });
+
 
 
 // Tabs 
@@ -57,3 +101,18 @@ const tabsNav = document.querySelectorAll('.bikes .tabs-nav__link'),
       tabsContent = document.querySelectorAll('.bikes .tabs-content');
 
 showTabs(tabsNav, tabsContent);
+
+
+// Burger
+
+const burgerBtnEl = document.querySelector('.burger');
+
+const handleBurgerClick = () => {
+  burgerBtnEl.classList.toggle('active');
+};
+
+document.addEventListener('click', (e)=>{
+  if(e.target.classList.contains('burger')) {
+    handleBurgerClick();
+  }
+});
